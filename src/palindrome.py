bcdcaeb85a6166434b9f72e6a520b12014278efa
Malena2317 <me.navarro@alumno.um.edu.ar>
2025-04-14 18:07:35 -0300

    Agragando implemntacion de comparacion y la salida del porgrama utilizando ctrl+c

--- src/palindrome.py
+++ src/palindrome.py
@@ -6,10 +6,15 @@ def is_palindrome(s):
 
 
 if __name__ == "__main__":
+
+    try:
     
-    entrada = input("Ingresá una palabra o frase: ")
-    if is_palindrome(entrada):
-        print("Es un palíndromo")
-    else:
-        print("No es un palíndromo")
+        entrada = input("Ingresá una palabra o frase: ")
+        if is_palindrome(entrada):
+            print("Es un palíndromo")
+        else:
+            print("No es un palíndromo")
+
+    except KeyboardInterrupt:
+        print("\nPrograma finalizado por el usuario con Ctrl + C")  #captura la interrupcion crtl+c y nuestra un mensaje amigable 
 
